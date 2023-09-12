d0dfaa433accbac19290fe0eabab1679f72f7582
Dmitry Kondratyev <dmitry.kondratyev@cern.ch>
2023-09-12 13:18:23 -0400

    fix style

--- src/index.ts
+++ src/index.ts
@@ -43,65 +43,64 @@ const extension: JupyterFrontEndPlugin<void> = {
     shutdown.addEventListener('click', () => {
       if (hubPrefix) {
         // For JupyterHub
-        commands.addCommand(customShutdown,
-          {
-            label: 'TEST Shut Down',
-            caption: 'TEST Shut down JupyterLab',
-            // isVisible: () => menu.quitEntry,
-            // isEnabled: () => menu.quitEntry,
-            execute: () => {
-              return showDialog({
-                title: 'TEST Shutdown confirmation',
-                body: 'TEST Please confirm you want to shut down JupyterLab.',
-                buttons: [
-                  Dialog.cancelButton(),
-                  Dialog.warnButton({ label: 'TEST Shut Down' })
-                ]
-              }).then(async result => {
-                if (result.button.accept) {
-                  const setting = ServerConnection.makeSettings();
-                  const apiURL = URLExt.join(setting.baseUrl, 'api/shutdown');
-                  // Shutdown all kernel and terminal sessions before shutting down the server
-                  // If this fails, we continue execution so we can post an api/shutdown request
-                  try {
-                    await Promise.all([
-                      app.serviceManager.sessions.shutdownAll(),
-                      app.serviceManager.terminals.shutdownAll()
-                    ]);
-                  } catch (e) {
-                    // Do nothing
-                    console.log(`TEST Failed to shutdown sessions and terminals: ${e}`);
-                  }
+        commands.addCommand(customShutdown, {
+          label: 'TEST Shut Down',
+          caption: 'TEST Shut down JupyterLab',
+          // isVisible: () => menu.quitEntry,
+          // isEnabled: () => menu.quitEntry,
+          execute: () => {
+            return showDialog({
+              title: 'TEST Shutdown confirmation',
+              body: 'TEST Please confirm you want to shut down JupyterLab.',
+              buttons: [
+                Dialog.cancelButton(),
+                Dialog.warnButton({ label: 'TEST Shut Down' })
+              ]
+            }).then(async result => {
+              if (result.button.accept) {
+                const setting = ServerConnection.makeSettings();
+                const apiURL = URLExt.join(setting.baseUrl, 'api/shutdown');
+                // Shutdown all kernel and terminal sessions before shutting down the server
+                // If this fails, we continue execution so we can post an api/shutdown request
+                try {
+                  await Promise.all([
+                    app.serviceManager.sessions.shutdownAll(),
+                    app.serviceManager.terminals.shutdownAll()
+                  ]);
+                } catch (e) {
+                  // Do nothing
+                  console.log(`TEST Failed to shutdown sessions and terminals: ${e}`);
+                }
         
-                  return ServerConnection.makeRequest(
-                    apiURL,
-                    { method: 'POST' },
-                    setting
-                  )
-                    .then(result => {
-                      if (result.ok) {
-                        // Close this window if the shutdown request has been successful
-                        const body = document.createElement('div');
-                        const p1 = document.createElement('p');
-                        p1.textContent = 'TEST You have shut down the Jupyter server. You can now close this tab.';
-                        const p2 = document.createElement('p');
-                        p2.textContent = 'TEST To use JupyterLab again, you will need to relaunch it.';
-        
-                        body.appendChild(p1);
-                        body.appendChild(p2);
-                        void showDialog({
-                          title: 'TEST Server stopped',
-                          body: new Widget({ node: body }),
-                          buttons: []
-                        });
-                        window.close();
-                      } else {
-                        throw new ServerConnection.ResponseError(result);
-                      }
-                    })
-                    .catch(data => {
-                      throw new ServerConnection.NetworkError(data);
-                    });
+                return ServerConnection.makeRequest(
+                  apiURL,
+                  { method: 'POST' },
+                  setting
+                )
+                  .then(result => {
+                    if (result.ok) {
+                      // Close this window if the shutdown request has been successful
+                      const body = document.createElement('div');
+                      const p1 = document.createElement('p');
+                      p1.textContent = 'TEST You have shut down the Jupyter server. You can now close this tab.';
+                      const p2 = document.createElement('p');
+                      p2.textContent = 'TEST To use JupyterLab again, you will need to relaunch it.';
+      
+                      body.appendChild(p1);
+                      body.appendChild(p2);
+                      void showDialog({
+                        title: 'TEST Server stopped',
+                        body: new Widget({ node: body }),
+                        buttons: []
+                      });
+                      window.close();
+                    } else {
+                      throw new ServerConnection.ResponseError(result);
+                    }
+                  })
+                  .catch(data => {
+                    throw new ServerConnection.NetworkError(data);
+                  });
                 }
               });
             }
