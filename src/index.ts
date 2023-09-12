ccf25fcba371c33d3927bc4c4af3799058db0b7b
Dmitry Kondratyev <dmitry.kondratyev@cern.ch>
2023-09-12 09:25:33 -0400

    open in the same tab

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import { Widget } from '@lumino/widgets';
 
 import { ITopBar } from 'jupyterlab-topbar';
 
+import { URLExt } from '@jupyterlab/coreutils';
+
 import '@jupyterlab/application/style/buttons.css';
 
 import '../style/index.css';
@@ -16,15 +18,30 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-shutdown:plugin',
   autoStart: true,
   requires: [IRouter, ITopBar],
-  activate: async (app: JupyterFrontEnd, router: IRouter, topBar: ITopBar) => {
+  activate: async (app: JupyterFrontEnd, paths: JupyterFrontEnd.IPaths, topBar: ITopBar) => {
     const { commands } = app;
+    let hubHost = '';
+    let hubPrefix = '';
+    if (paths.urls) {
+      hubHost = paths.urls.hubHost;
+      hubPrefix = paths.urls.hubPrefix;
+    }
+    const controlPanelSameTab = 'hub:control-panel-same-tab'
+
     const shutdown = document.createElement('a');
     shutdown.id = 'shutdown';
     shutdown.innerHTML = 'Shut Down';
     shutdown.addEventListener('click', () => {
-      if (commands.hasCommand('hub:control-panel')) {
+      if (hubPrefix) {
         // For JupyterHub
-        commands.execute('hub:control-panel');
+        commands.addCommand(controlPanelSameTab, {
+          label: 'Hub Control Panel',
+          caption: 'Open the Hub control panel in the same browser tab',
+          execute: () => {
+            window.open(hubHost + URLExt.join(hubPrefix, 'home'), '_self');
+          }
+        });
+        commands.execute(controlPanelSameTab);
       } else {
         // For basic JupyterLab w/o JupyterHub integration
         commands.execute('filemenu:shutdown');
