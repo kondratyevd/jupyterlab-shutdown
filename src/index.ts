ce6eaaee14bbfd3f79f2ce955673dc19bf099734
Dmitry Kondratyev <dmitry.kondratyev@cern.ch>
2023-09-13 12:28:21 -0400

    fix style

--- src/index.ts
+++ src/index.ts
@@ -22,11 +22,7 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-shutdown:plugin',
   autoStart: true,
   requires: [IRouter, ITopBar],
-  activate: async (
-    app: JupyterFrontEnd,
-    router: IRouter,
-    topBar: ITopBar
-  ) => {
+  activate: async (app: JupyterFrontEnd, router: IRouter, topBar: ITopBar) => {
     const { commands } = app;
     const customShutdown = 'hub:custom-shutdown';
     commands.addCommand(customShutdown, {
@@ -40,57 +36,58 @@ const extension: JupyterFrontEndPlugin<void> = {
             Dialog.cancelButton(),
             Dialog.warnButton({ label: 'Shut Down' })
           ]
-        }).then(async result => {
-          if (result.button.accept) {
-            const setting = ServerConnection.makeSettings();
-            const apiURL = URLExt.join(setting.baseUrl, 'api/shutdown');
-            // Shutdown all kernel and terminal sessions before shutting down the server
-            // If this fails, we continue execution so we can post an api/shutdown request
-            try {
-              await Promise.all([
-                app.serviceManager.sessions.shutdownAll(),
-                app.serviceManager.terminals.shutdownAll()
-              ]);
-            } catch (e) {
-              // Do nothing
-              console.log(`Failed to shutdown sessions and terminals: ${e}`);
+        })
+          .then(async result => {
+            if (result.button.accept) {
+              const setting = ServerConnection.makeSettings();
+              const apiURL = URLExt.join(setting.baseUrl, 'api/shutdown');
+              // Shutdown all kernel and terminal sessions before shutting down the server
+              // If this fails, we continue execution so we can post an api/shutdown request
+              try {
+                await Promise.all([
+                  app.serviceManager.sessions.shutdownAll(),
+                  app.serviceManager.terminals.shutdownAll()
+                ]);
+              } catch (e) {
+                // Do nothing
+                console.log(`Failed to shutdown sessions and terminals: ${e}`);
+              }
+
+              return ServerConnection.makeRequest(
+                apiURL,
+                { method: 'POST' },
+                setting
+              )
+                .then(result => {
+                  if (result.ok) {
+                    // Close this window if the shutdown request has been successful
+                    const body = document.createElement('div');
+                    const p1 = document.createElement('p');
+                    p1.textContent = 'You have shut down the session.';
+                    const p2 = document.createElement('p');
+                    p2.textContent =
+                      'You will now be redirected to the Analysis Facility starting page.';
+
+                    body.appendChild(p1);
+                    body.appendChild(p2);
+                    void showDialog({
+                      title: 'Session closed.',
+                      body: new Widget({ node: body }),
+                      buttons: []
+                    });
+                    window.close();
+                  } else {
+                    throw new ServerConnection.ResponseError(result);
+                  }
+                })
+                .catch(data => {
+                  throw new ServerConnection.NetworkError(data);
+                });
             }
-
-            return ServerConnection.makeRequest(
-              apiURL,
-              { method: 'POST' },
-              setting
-            )
-              .then(result => {
-                if (result.ok) {
-                  // Close this window if the shutdown request has been successful
-                  const body = document.createElement('div');
-                  const p1 = document.createElement('p');
-                  p1.textContent =
-                    'You have shut down the session.';
-                  const p2 = document.createElement('p');
-                  p2.textContent =
-                    'You will now be redirected to the Analysis Facility starting page.';
-
-                  body.appendChild(p1);
-                  body.appendChild(p2);
-                  void showDialog({
-                    title: 'Session closed.',
-                    body: new Widget({ node: body }),
-                    buttons: []
-                  });
-                  window.close();
-                } else {
-                  throw new ServerConnection.ResponseError(result);
-                }
-              })
-              .catch(data => {
-                throw new ServerConnection.NetworkError(data);
-              });
-          }
-        }).then(() => {
-          router.navigate('/hub/home/', { hard: true });
-        });
+          })
+          .then(() => {
+            router.navigate('/hub/home/', { hard: true });
+          });
       }
     });
 
